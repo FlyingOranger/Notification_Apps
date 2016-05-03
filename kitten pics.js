c12b370b98c9ccefb3d7d9afe25f844a9a31b9f7
Joel Barna <joel.tbarna@gmail.com>
2016-05-03 13:44:02 -0400

    Lots of comments and instructions

--- kitten pics.js
+++ kitten pics.js
@@ -1,35 +1,93 @@
-module.exports = function( kittenData, helper){
+/* This Crawler checks to see if there are any new posts on r/aww, 
+   with "kitten" in their title, and with a score of at least 50
+   
+   We save the last 4 posts we've found, because if some posts are really close in score,
+   they can overtake eachother and you'll have lots of notifications of something you might 
+   have seen already.
+   
+*/
+module.exports = function( data , helper){
 
-    if ( kittenData.data.children.length > 0){
+/* If you're not familiar with the Reddit API and JSON, do these following steps:
+
+    1) open https://www.reddit.com/r/pics.json in your web browser
+    2) Copy all the scrambled text you see on screen with Ctrl-A, Ctrl-C
+    3) open http://jsonprettyprint.com/ and paste the scrambled text in there
+
+    What you see is a regular JSON response from Reddit
+
+* In order to make coding a little easier, RedditCanFly only passes the most important information to this function
+    by advancing to the JSON response's data.children field, and each child advanced to the child.data field
+
+    Long story short, this means that data is an array of reddit posts.
+
+* So if you accessed the first post, at say data[0], it would look like this:
+{
+    id:         a unique id just for this post,
+    score:      reddit's score for this post,
+    is_self:    true / false,
+    title:      the title of this post,
+    url:        link of this post
+
+    there are more as well, just do the beginning instructinos to find what information there is
+}
+
+* Also to make coding easier, this function is provided with a helper object
+
+    helper.set( dataName, dataToSave )
+        - Saves dataToSave inbetween requests, with the specified dataName
+
+    helper.get( dataName )
+        - Returns the dataToSave inbetween requests from the specified dataName
+
+    helper.more() 
+        - Calls your function again, but with 100 more posts
+
+    helper.notify( Title, Link )
+        - Makes the orange envelope fly across your screen! 
+            - Title is what is shown in flying banner along with notification in task bar
+            - Link is what is your browser opens to when the user clicks on the notification
+                - If link is empty, then only the banner flies and no notification is put in the task tray
+                - If the link matches another link already in the task tray, 
+                    then the banner is flown but not added to the tray (avoid duplicates)
+*/   
+ 
+    
+    // Always check to see if there are any posts
+    if ( data.length > 0){
         
+        // get the last posts we have found
         var lastPosts = helper.get('last-posts');
+        
+        // if this is the first time, and we haven't found any posts, set it to an empty array
         if (lastPosts == null)
             lastPosts = [];
-        
-        var findLatestPost = function( kData ){
             
-            for (var post of kData.data.children){
-                post = post.data;
-                if (post.score >= 50){
+        // go through all the posts
+        for (var post of data){
+            
+            if (post.score >= 200){
+                
+                // Is this post's ID in last 4 posts?
+                if (lastPosts.indexOf(post.id) < 0){
+
+                    // it is, so add it to the beginning of our array 
+                    lastPosts.unshift(post.id);
+                    lastPosts.length = 4; // and by making the length 4, we chop off any remaining items
+
+                    // save lastPosts for next time
+                    helper.set('last-posts', lastPosts);
                     
-                    if (lastPosts.indexOf(post.name) < 0){
-                        
-                        // found our new high post
-                        //console.log("Found new high post ", post.name, post.url);
-                        lastPosts.unshift(post.name);
-                        lastPosts.length = 4;
-                        
-                        helper.set('last-posts', lastPosts);
-                        helper.notify("New Kitten!", post.url);
-                    }
-                    
-                    return;
+                    // notify the user that there is a new kitten post over 50!
+                    helper.notify("New Kitten!", post.url);
                 }
+                
+                // make sure to exit, or else we'll call helper.more by accident
+                return;
             }
-            
-            helper.more(findLatestPost);
-        };
+        }
         
-        findLatestPost(kittenData);
+        // we went through all the posts but didn't find any over 50... get some more posts
+        helper.more();
     }
 }
--- oranger.js
+++ oranger.js
@@ -1,20 +1,84 @@
-module.exports = function(messageData, helper){
+/* This app notifies you if you have any new unread messages,
+    which is anytime you log into Reddit and see an orange envelope
+    waiting for you.
+*/
 
-    if (messageData.data.children.length > 0){
+module.exports = function( data, helper){
+
+/* If you're not familiar with the Reddit API and JSON, do these following steps:
+
+    1) open https://www.reddit.com/r/pics.json in your web browser
+    2) Copy all the scrambled text you see on screen with Ctrl-A, Ctrl-C
+    3) open http://jsonprettyprint.com/ and paste the scrambled text in there
+
+    What you see is a regular JSON response from Reddit
+
+* In order to make coding a little easier, RedditCanFly only passes the most important information to this function
+    by advancing to the JSON response's data.children field, and each child advanced to the child.data field
+
+    Long story short, this means that data is an array of reddit posts.
+
+* So if you accessed the first post, at say data[0], it would look like this:
+{
+    id:         a unique id just for this post,
+    score:      reddit's score for this post,
+    is_self:    true / false,
+    title:      the title of this post,
+    url:        link of this post
+
+    there are more as well, just do the beginning instructinos to find what information there is
+}
+
+* Also to make coding easier, this function is provided with a helper object
+
+    helper.set( dataName, dataToSave )
+        - Saves dataToSave inbetween requests, with the specified dataName
+
+    helper.get( dataName )
+        - Returns the dataToSave inbetween requests from the specified dataName
+
+    helper.more() 
+        - Calls your function again, but with 100 more posts
+
+    helper.notify( Title, Link )
+        - Makes the orange envelope fly across your screen! 
+            - Title is what is shown in flying banner along with notification in task bar
+            - Link is what is your browser opens to when the user clicks on the notification
+                - If link is empty, then only the banner flies and no notification is put in the task tray
+                - If the link matches another link already in the task tray, 
+                    then the banner is flown but not added to the tray (avoid duplicates)
+*/   
+
+
+    // always make sure we have posts
+    if (data.length > 0){
         
+        // get the latest message, which is the first item in our array
+        var latestMessage = data[0];
+        
+        // get the last message we've seen, which we saved with our helper
         var lastName = helper.get('last_name');
-        var latestMessage = messageData.data.children[0].data;
         
-        if (latestMessage.name !== lastName){
+        // Is the latest message the last message we've seen?
+        if (latestMessage.id !== lastName){
             
-            helper.set('last_name', latestMessage.name);
+            // houston, we have a new messsage. Save it's ID for later
+            helper.set('last_name', latestMessage.id);
             
-            if (latestMessage.subject !== "post reply" &&
-                latestMessage.subject !== "comment reply")
-                helper.notify("New Message!", "https://www.reddit.com/message/unread/");
+            // create the title for our banner
+            var title;
+            if (latestMessage.subject === "post reply") 
+                title = "Post Reply!";
             
-            else 
-                helper.notify(latestMessage.subject, "https://www.reddit.com/message/unread/");
+            else if (latestMessage.subject === "comment reply")
+                title = "Comment Reply!";
+            
+            else title = "New Message!";
+                
+            // send the notification flying across the screen! 
+            // Note: if we already have a notification sitting in the tray
+            // with this link, it will fly the banner but not add to the tray
+            helper.notify(title, "https://www.reddit.com/message/unread/");
             
         }
     }
