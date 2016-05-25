5ad03b71afe0a790c88d055aa90782247c7b5369
Joel Barna <joel.tbarna@gmail.com>
2016-05-24 22:15:31 -0400

    Erased comments in oranger

--- Oranger.js
+++ Oranger.js
@@ -3,49 +3,6 @@
     waiting for you.
 */
 
-/* If you're not familiar with the Reddit API and JSON, do these following steps:
-
-    1) open https://www.reddit.com/r/pics.json in your web browser
-    2) Copy all the scrambled text you see on screen with Ctrl-A, Ctrl-C
-    3) open http://jsonprettyprint.com/ and paste the scrambled text in there
-
-    What you see is a regular JSON response from Reddit
-
-* In order to make coding a little easier, Flying Oranger only passes the most important information to this function
-    by advancing to the JSON response's data.children field, and each child advanced to the child.data field
-
-    Long story short, this means that data is an array of reddit posts.
-
-* So if you accessed the first post, at say data[0], it would look like this:
-{
-    id:         a unique id just for this post,
-    score:      reddit's score for this post,
-    is_self:    true / false,
-    title:      the title of this post,
-    url:        link of this post
-
-    there are more as well, just do the beginning instructinos to find what information there is
-}
-
-* Also to make coding easier, this function is provided with a helper object
-
-    helper.set( dataName, dataToSave )
-        - Saves dataToSave inbetween requests, with the specified dataName
-
-    helper.get( dataName )
-        - Returns the dataToSave inbetween requests from the specified dataName
-
-    helper.more() 
-        - Calls your function again, but with 100 more posts
-
-    helper.notify( Title, Link )
-        - Makes the orange envelope fly across your screen! 
-            - Title is what is shown in flying banner along with notification in task bar
-            - Link is what is your browser opens to when the user clicks on the notification
-                - If link is empty, then only the banner flies and no notification is put in the task tray
-                - If the link matches another link already in the task tray, 
-                    then the banner is flown but not added to the tray (avoid duplicates)
-*/   
 module.exports = {
     
     scope: "privatemessages",
